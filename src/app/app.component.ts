2eb4a28b93d72fdd6ef094abe2b7abe326cfff7b
Alex <erzielen@yahoo.com>
2021-09-29 21:58:11 -0500

    One way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  // styleUrls: [ 'app.component.css']
 })
 export class AppComponent {
-  title = 'Basics';
+  title:string = 'Contador App';
+  numero:number= 0;
+  base:number= 5;
+  // sumar(){
+  //   this.numero+=1;
+  // }
+  // restar(){
+  //   this.numero-=1;
+  // }
+  acumular ( valor: number){
+    this.numero+=valor;
+  }
+
 }
