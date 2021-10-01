34a946ece6563115d554d6db0344798d7adc1b28
Alex <erzielen@yahoo.com>
2021-09-30 21:33:09 -0500

    Directiva ngIf

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent  { 
   heroes:string []= ['Spiderman','Ironman','Hulk','Thor']; 
-  heroeBorrado:string='';
+  heroeBorrado:string=' ';
 
 borrarHeroe(){
   
