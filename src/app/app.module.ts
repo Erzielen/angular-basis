c4099ff66a2a1beebac838c2d71ecdceabec27dc
Alex <erzielen@yahoo.com>
2021-10-02 10:01:43 -0500

    Creacion del modulo Contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contadorComponent } from './contador/contador.component';
+import { contadorModule } from './contador/contador.module';
 import { heroesModule } from './heroes/heroes.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    contadorComponent
+    AppComponent
     
   ],
   imports: [
     BrowserModule,
-    heroesModule
+    heroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
